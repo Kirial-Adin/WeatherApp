86c14ccaa25a064b235d44c89edf22f72fe18981
Kirial-Adin <kirial.adin@yandex.ru>
2024-03-16 12:20:59 +0300

    Update Highlights.vue

--- WeatherApp/src/components/Highlights.vue
+++ WeatherApp/src/components/Highlights.vue
@@ -1,8 +1,14 @@
 <script setup>
+const props = defineProps({
+  weatherInfo: {
+    type: [Object, null],
+    required: true,
+  }
+})
 </script>
 
 <template>
-    <div class="section highlights">
+    <div v-if="weatherInfo?.weather" class="section highlights">
         <div class="title">
         Today's Highlights
         </div>
@@ -17,45 +23,45 @@
                     <div class="card-justify">
                     <div class="info-main">
                         <div class="info-main-num">
-                        3.6
+                          {{ weatherInfo?.wind?.speed }}
                         </div>
                         <div class="info-main-text">
-                        m/s
+                          m/s
                         </div>
                     </div> 
                     <div class="info-main">
                         <div class="info-main-num">
-                        350
+                          {{ weatherInfo?.wind?.deg }}
                         </div>
                         <div class="info-main-text">
-                        deg
+                          deg
                         </div>
                     </div>
                     </div>
                 </div>
                 </div>
                 <div class="card-small">
-                <div class="card-small-title">
-                    Wind gusts
-                </div>
-                <div class="card-small-info">
-                    <div class="card-small-data">
-                    <div class="info-main-num">
-                        8.4
+                  <div class="card-small-title">
+                      Wind gusts
+                  </div>
+                  <div class="card-small-info">
+                      <div v-if="weatherInfo?.wind?.gust" class="card-small-data">
+                        <div class="info-main-num">
+                          {{ Math.round(weatherInfo?.wind?.gust) }}
+                        </div>
+                        <div class="info-main-text">
+                            m/s
+                        </div>
+                      </div>
+                      <div class="card-small-hint">
+                      <div class="card-small-pic card-small-pic--wind"></div>
+                        <div class="card-small-text text-egorova">
+                            Learn
+                            <a href="https://www.windy.com/articles/weather-phenomena-what-s-the-difference-between-sustained-winds-and-wind-gusts-10390?satellite,7.787,115.115,5" target="_blank">more</a>
+                            about gusts
+                        </div>
                     </div>
-                    <div class="info-main-text">
-                        m/s
-                    </div>
-                    </div>
-                    <div class="card-small-hint">
-                    <div class="card-small-pic card-small-pic--wind"></div>
-                    <div class="card-small-text text-egorova">
-                        Learn
-                        <a href="https://www.windy.com/articles/weather-phenomena-what-s-the-difference-between-sustained-winds-and-wind-gusts-10390?satellite,7.787,115.115,5" target="_blank">more</a>
-                        about gusts
-                    </div>
-                    </div>
-                </div>
+                  </div>  
                 </div>
             </div>
             <div class="highlight">
